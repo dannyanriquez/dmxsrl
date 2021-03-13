d42cad358628c49884617b0252bc11a012e049c8
DannyAnriquez <anriquezda@gmail.com>
2021-03-13 00:12:38 -0300

    changue menu

--- js/function.js
+++ js/function.js
@@ -27,3 +27,11 @@ $(document).ready(function() {
     interval: 3000,
  })
     
+
+ $(document).ready(function () {
+    $('.navbar-light .dmenu').hover(function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+        }, function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+        });
+    });
