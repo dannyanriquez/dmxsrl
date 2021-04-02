70db745d519bac9f2f55ced6b02bab5bda5b2b97
DannyAnriquez <anriquezda@gmail.com>
2021-04-02 15:36:29 -0300

    cambios finales

--- js/function.js
+++ js/function.js
@@ -38,3 +38,16 @@ $(document).ready(function() {
             $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
         });
     });
+
+    /*BOTON IR ARRIBA*/ 
+    $(document).ready(function(){ //Hacia arriba
+        irArriba();
+      });
+      
+      function irArriba(){
+        $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },100); });
+        $(window).scroll(function(){
+          if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
+        });
+        $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+      }
